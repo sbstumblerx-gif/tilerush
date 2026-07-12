2897cd4ccdb7b455024b2c6d470fa118d36eb51e
sbstumblerx-gif <sb.stumblerx@gmail.com>
2026-07-12 19:58:21 +0300

    Refactor PlayPage to use createFileRoute

--- src/routes/play.tsx
+++ src/routes/play.tsx
@@ -1,7 +1,14 @@
-export default function PlayPage() {
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/play")({
+  component: PlayPage,
+});
+
+function PlayPage() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>Pelisivu</h1>
+      <p>Reitti tunnistettu onnistuneesti.</p>
     </div>
   );
 }
