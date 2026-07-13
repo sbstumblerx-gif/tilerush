691839b74fdbf8cbd05bdb56bcec74a7216eb2a0
sbstumblerx-gif <sb.stumblerx@gmail.com>
2026-07-13 12:29:00 +0300

    Refactor start.ts to use createStartHandler

--- src/start.ts
+++ src/start.ts
@@ -1,24 +1,10 @@
-import { createStart, createMiddleware } from "@tanstack/react-start";
+import { createStartHandler, defaultRenderHandler } from '@tanstack/react-start/server'
+import { createRouter } from './router'
 
-import { renderErrorPage } from "./lib/error-page";
-import { attachSupabaseAuth } from "@/integrations/supabase/auth-attacher";
-
-const errorMiddleware = createMiddleware().server(async ({ next }) => {
-  try {
-    return await next();
-  } catch (error) {
-    if (error != null && typeof error === "object" && "statusCode" in error) {
-      throw error;
-    }
-    console.error(error);
-    return new Response(renderErrorPage(), {
-      status: 500,
-      headers: { "content-type": "text/html; charset=utf-8" },
-    });
-  }
-});
-
-export const startInstance = createStart(() => ({
-  functionMiddleware: [attachSupabaseAuth],
-  requestMiddleware: [errorMiddleware],
-}));
+// Luodaan standardi Start-käsittelijä, jota Vinxi osaa lukea
+export default createStartHandler({
+  createRouter,
+  getRouterManifest: () => ({
+    routes: {},
+  }),
+})(defaultRenderHandler)
